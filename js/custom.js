a929882345d2e552861883d564c23c2f0c500c35
Jishil <jishil@spericorn.com>
2024-01-09 20:13:16 +0530

    home page complated

--- js/custom.js
+++ js/custom.js
@@ -3,3 +3,44 @@ $(document).ready(function () {
 		$('.body-wrapper').toggleClass('nav-collapse');
 	});
 });
+
+// roadmap slider
+$('.roadmap-slider').slick({
+	slidesToShow: 3,
+	slidesToScroll: 3,
+	prevArrow: ".btn-prev",
+	nextArrow: ".btn-next",
+	speed: 600,
+	responsive:[
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2,
+			}
+		},
+		{
+			breakpoint: 580,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+			}
+		},
+
+	]
+});
+// roadmap slider ends
+
+// Video slider
+
+$('.video-slider').slick({
+	dots: true,
+	infinite: false,
+	speed: 300,
+	dots: false,
+	slidesToShow: 1,
+	fade: true,
+  	cssEase: 'linear',
+	prevArrow: $('.video-prev-btn'),
+	nextArrow: $('.video-next-btn')
+});
